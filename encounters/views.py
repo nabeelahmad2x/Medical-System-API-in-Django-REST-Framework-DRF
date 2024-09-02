4727ce2fb6320469bf8ff93a289c9e414d7ee4eb
nabeelahmad2x <102218637+nabeelahmad2x@users.noreply.github.com>
2024-09-02 21:37:16 +0500

    added custom auth decorator for admin access in encounters

--- encounters/views.py
+++ encounters/views.py
@@ -1,22 +1,58 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 
-from rest_framework.authentication import BasicAuthentication 
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+from custom_permission_classes.permissions import admin_auth_required
 from .models import Appointment, Encounter
 from .serializers import AppointmentSerializer, EncounterSerializer
 
-# @authentication_classes([BasicAuthentication]) 
-# @permission_classes([IsAuthenticated]) 
+
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
 
-# @authentication_classes([BasicAuthentication]) 
-# @permission_classes([IsAuthenticated]) 
 class EncounterViewSet(viewsets.ModelViewSet):
     queryset = Encounter.objects.all()
     serializer_class = EncounterSerializer
 
+
+@api_view(['GET'])
+@admin_auth_required
+def get_encounter_details(request, encounter_id):
+
+    encounter = Encounter.objects.get(pk=encounter_id)
+
+    if encounter:
+        appointment = encounter.appointment
+        doctor = appointment.doctor
+        patient = appointment.patient
+        medicine = encounter.prescription
+
+        return Response({
+            f"Encounter ID: {encounter.id},"
+            f"Notes: {encounter.notes},"
+            f"Prescription: {medicine},"
+            f"Appointment Details:"
+            f"  Appointment ID: {appointment.id},"
+            f"  Date & Time: {appointment.appointment_datetime},"
+            f"Doctor Details:"
+            f"  Doctor ID: {doctor.id},"
+            f"  Name: {doctor.name},"
+            f"  Contact: {doctor.contact},"
+            f"  Email: {doctor.email},"
+            f"  Designation: {doctor.designation},"
+            f"  Speciality: {doctor.speciality}\n"
+            f"Patient Details:"
+            f"  Patient ID: {patient.id},"
+            f"  Name: {patient.name},"
+            f"  Contact: {patient.contact},"
+            f"  Email: {patient.email},"
+            f"  Date of Birth: {patient.date_of_birth},"
+            f"  Blood Group: {patient.blood_group},"
+            f"  Disease Stage: {patient.disease_stage}."
+        })
+    else:
+        return Response({"404: Encounter not Found :D"}, status=status.HTTP_404_NOT_FOUND)
--- user_login/views.py
+++ user_login/views.py
@@ -8,19 +8,15 @@ from rest_framework.permissions import AllowAny
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
+
     def post(self, request):
-        #print('point 1-------------------------------------')
         email = request.data.get('email')
         password = request.data.get('password')
 
         user = authenticate(request, username=email, password=password)
-        #print('point 2-------------------------------------')
-
         if user:
-            #print('point 3-------------------------------------')
             refresh = RefreshToken.for_user(user)
-            #print('point 4-------------------------------------')
-            return Response({'refresh token': str(refresh), 'access token': str(refresh.access_token)})        
+            return Response({'refresh token': str(refresh), 'access token': str(refresh.access_token)})
         return Response({'error': 'Invalid email or password'})
 
 
@@ -34,5 +30,3 @@ class RefreshView(APIView):
         if refresh_token:
             return Response({'new access token': {str(refresh_token.access_token)}})
         return Response({'error': 'invalid refresh token'})
-    
-    
